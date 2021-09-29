e859b9604a7901a70047a13bcccbd1213905e157
Rosa Amalia <68428942+rosaamalia@users.noreply.github.com>
2021-09-30 01:00:25 +0700

    update pw

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-    $db = pg_connect("host=localhost port=5432 dbname=fp-mbd user=postgres password=05150302");
+    $db = pg_connect("host=localhost port=5432 dbname=fp-mbd user=postgres password=password");
 ?>
